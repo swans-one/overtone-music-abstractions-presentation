1062ed4a92f1cf3aaf5d57255b69b2c2ab21e10d
Erik Swanson <theerikswanson@gmail.com>
2014-10-10 21:08:09 -0400

    Full practice run through.

--- src/music_abstractions/full.clj
+++ src/music_abstractions/full.clj
@@ -1,84 +1,96 @@
 (ns music-abstractions.full
   (:use overtone.live))
 
-; S.1 Waves
-; ---------
+;; (demo 5 (pan2 (+ (sin-osc 440) (sin-osc (* 440 2)))))
 
-; Sine waves and composite waves
+(definst chime [freq 440 dur 1]
+  (let [fund freq
+        over1 (* freq 2)
+        over2 (* freq 3.1)
+        over3 (* freq 6.2)]
+    (* (pan2 (+ (* (sin-osc fund) 1)
+                (* (sin-osc over1) 0.7)
+                (* (sin-osc over2) 0.2)
+                (* (sin-osc over3) 0.1)))
+       (env-gen (lin 0 0.1 dur))
+       0.5)))
 
-(demo 3 (pan2 (sin-osc 440)))
+;; (chime 440)
 
-(demo 3 (pan2 (saw 440)))
+;; (stop)
 
-(demo 3 (pan2 (lf-tri:ar 440)))
+;; (demo 4 (pan2 (white-noise)))
 
-(demo 3 (pan2 (lf-pulse:ar 440)))
+;; (demo 4 (pan2 (* (env-gen (lin 1 1 1)) (sin-osc 440))))
+
+(definst hi-hat [strum 0.1]
+  (pan2 (* (env-gen (perc 0 strum) :action FREE) 
+           (hpf (white-noise) 9000))))
+
+;; (hi-hat)
+
+(definst bass [freq 110 amp 0.8 dur 5]
+  (let [fundamental-fq freq
+        overtones [1 2 3 4 5 6 7 8]
+        weights [1 0.1 0.8 0.7 0.6 0.7 0.4 0.2]
+        fqs (map (partial * fundamental-fq) overtones)
+        total-weight (* 0.2 amp)]
+    (pan2 (* (sum (map * (map sin-osc fqs) weights))
+             (env-gen (lin (* dur 2/5) (* dur 1/5) (* dur 2/5)) :action FREE)
+             total-weight))))
+
+;; (bass (midi->hz 48) 2 7)
+
+(definst choir [freq 880 dur 7]
+  (let [fund freq
+        fifth (* freq 3/2)
+        vib_fq 5 vib_mag 13
+        trem_fq 1 trem_mag 0.1]
+    (pan2 (* (+ (lf-tri (+ fund (* vib_mag (sin-osc vib_fq))) 
+                        (+ fifth (* vib_mag (sin-osc vib_fq)))))
+             (env-gen (lin 0.2 0.2 dur))
+             (+ 1 (* (sin-osc trem_fq) trem_mag))
+             (* 0.25 (sin-osc 0.5))))))
+
+;; (choir)
+
+(defn play-pattern [cur-t sep-t seq sound]
+  (at cur-t (when (first seq) (apply sound (first seq))))
+  (let [new-t (+ cur-t sep-t)]
+    (apply-by new-t #'play-pattern [new-t sep-t (rest seq) sound])))
 
 
-; S.2 Instruments
-; ---------------
+(defn play-all [sep-t patterns]
+  (let [t (+ (now) 200)]
+    (doseq [[sound pattern] patterns]
+      (play-pattern t sep-t pattern sound))))
 
-; Composite waves + timber + inharmonics
 
-; http://en.wikipedia.org/wiki/Harmonic
-; http://en.wikipedia.org/wiki/Timbre
-; http://en.wikipedia.org/wiki/Inharmonicity
+(def bpm 200)
+(def beat-dur (/ 60 bpm))
 
-; http://www.themeandvariations.org/Images/other/fourier_flute_violin.png
+(def hat-seq [[] [0.5] nil nil nil [] nil [] [] nil nil]) ;; 5
 
-(def f-freq 440)
-(demo 3 (pan2 (sin-osc f-freq)))
-(demo 3 (pan2 (* 0.232 (+ (sin-osc f-freq)
-                          (* (sin-osc (* f-freq 2)) 0.6)
-                          (* (sin-osc (* f-freq 3)) 0.6)
-                          (* (sin-osc (* f-freq 4)) 0.7)
-                          (* (sin-osc (* f-freq 5)) 0.5)
-                          (* (sin-osc (* f-freq 6)) 0.2)
-                          (* (sin-osc (* f-freq 7)) 0.5)
-                          (* (sin-osc (* f-freq 8)) 0.2)
-))))
+(def bass-seq (concat [[:freq (midi->hz 48) :dur (* 12 beat-dur)]]
+                      (repeat 6 nil)
+                      [[:freq (midi->hz 55) :dur (* 12 beat-dur)]]
+                      (repeat 5 nil)
+                      [[:freq (midi->hz 52) :dur (* 12 beat-dur)]]
+                      (repeat 3 nil))) ;; 17
 
-(demo 3 (pan 2 (map sin-osc)))
+(def chime-seq
+  (repeatedly #(if-let [a-note (rand-nth [60 64 67 70 72 76 79 nil])]
+                 (vector (midi->hz a-note) :dur (* 1.5 beat-dur))
+                 nil)))
 
-(demo 3 (pan2 (* 0.3984(+ (sin-osc f-freq)
-                          (* (sin-osc (* f-freq 2)) 1.0)
-                          (* (sin-osc (* f-freq 3)) 0.1)
-                          (* (sin-osc (* f-freq 4)) 0.2)
-                          (* (sin-osc (* f-freq 5)) 0.15)
-                          (* (sin-osc (* f-freq 6)) 0.03)
-                          (* (sin-osc (* f-freq 7)) 0.02)
-                          (* (sin-osc (* f-freq 8)) 0.01)
-))))
+(def choir-seq (repeatedly #(if (< 0.90 (rand))
+                              [(midi->hz (rand-nth [72 82 88]))
+                               (rand-int 8)]
+                              nil)))
 
-(demo 3 
-      (+ (map 
-         (fn [harm weight] (* (sin-osc (* f-freq harm)) weight)) 
-         [1 2 3 4 5] 
-         [1 0.5 0.2 0.4 0.1])))
-
-(defn weighted-harmonic-osc [fundamental harmonic weight]
-  (with-overloaded-ugens
-    (pan2 (* (sin-osc (* fundamental harmonic)) weight))))
-
-(demo 3 (weighted-harmonic-osc 220 1 0.5))
-
-(defn instrument [fundamental harmonics weights]
-  (let [total-weight (/ 1 (sum weights))
-        osc (partial weighted-harmonic-osc fundamental)]
-    (with-overloaded-ugens
-      (pan2 (* total-weight (sum (map osc harmonics weights)))))))
-
-(demo 3 (instrument 220 [1 2 3 4] [1 1 0.5 0.2]))
-
-(definst five-ovr [freq 440 dur 3]
-  (let [one   (sin-osc freq)
-        two   (sin-osc (* freq 2))
-        three (sin-osc (* freq 3))
-        four  (sin-osc (* freq 4))
-        five  (sin-osc (* freq 5))
-        env (env-gen (lin 0.1 dur 0.1))]
-    (* (+ one two three four five) env)))
-
-(five-ovr 440)
-(demo 3 (pan2 (sin-osc 440)))
-
+(play-all 300 {hi-hat (cycle hat-seq)
+               chime chime-seq
+               bass (cycle bass-seq)
+               choir choir-seq})
+                      
+(stop)
